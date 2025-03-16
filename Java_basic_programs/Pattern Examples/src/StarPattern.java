0c0deb800a705ecb5b303821260e11b56b990bfe
Shital Shankarlal Nishad <102948948+shital4197@users.noreply.github.com>
2025-03-16 22:21:03 +0530

    Add files via upload

---
+++ Java_basic_programs/Pattern Examples/src/StarPattern.java
@@ -0,0 +1,27 @@
+public class StarPattern {
+
+    public static void main(String[] args) {
+
+//        for(int i = 1; i<=5; i++){
+//            for(int j = 1; j<=i;j++){
+//                System.out.print(" *  ");
+//            }
+//            System.out.println( );
+//        }
+
+//        for(int i = 1 ; i<=5; i++){
+//            for(int j = 1; j<=5; j++){
+//                System.out.print(" * ");
+//            }
+//            System.out.println(" * ");
+//        }
+
+        for(int i = 1; i<=4; i++){
+            for(int j = 4; j>=i; j--){
+                System.out.print(" * ");
+            }
+            System.out.println( );
+        }
+
+    }
+}
